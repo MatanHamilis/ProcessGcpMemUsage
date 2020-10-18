932878abe23d561f4bf325b63fbed9585559e47f
Matan Hamilis <matan.hamilis@gmail.com>
2020-10-19 00:44:20 +0300

    parsing files now

--- src/main.go
+++ src/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"container/list"
 	"github.com/golang/protobuf/jsonpb"
+	"io/ioutil"
 	"log"
 	"os"
+	"path"
+	"strings"
 )
 
 func ParseCollectionEvents(c chan CollectionEvent, s *bufio.Scanner) {
@@ -17,21 +21,64 @@ func ParseCollectionEvents(c chan CollectionEvent, s *bufio.Scanner) {
 	close(c)
 }
 
-func ParseInstanceEvents(c chan InstanceEvent, s *bufio.Scanner) {
-	var v InstanceEvent
+func ParseInstanceUsage(c chan InstanceUsage, s *bufio.Scanner) {
+	var v InstanceUsage
+	i := 0
 	for s.Scan() != false {
 		jsonpb.UnmarshalString(s.Text(), &v)
+		c <- v
+		i++
 	}
 	close(c)
 }
 
-func main() {
-	f, _ := os.Open("../stam.json.gz")
-	t, _ := gzip.NewReader(f)
-	b := bufio.NewScanner(t)
-	c := make(chan CollectionEvent)
-	go ParseCollectionEvents(c, b)
-	for e := range c {
-		log.Println(*e.Time)
+func IterateFilesInDir(p string, filter string, c chan string) {
+	d, _ := ioutil.ReadDir(p)
+	for _, f := range d {
+		if f.IsDir() {
+			continue
+		}
+		if !strings.Contains(f.Name(), filter) {
+			continue
+		}
+		c <- path.Join(p, f.Name())
 	}
+	close(c)
+}
+
+func GenerateGzipScanner(path string) *bufio.Scanner {
+	f, _ := os.Open(path)
+	t, _ := gzip.NewReader(f)
+	return bufio.NewScanner(t)
+}
+
+func GenerateHistogramFromStream(c chan InstanceUsage, end chan bool) map[uint64][]uint64 {
+	m := make(map[uint64][]uint64)
+	for l := range c {
+		log.Println(*l.StartTime)
+	}
+	end <- true
+	return m
+}
+
+func ProcessEachInstanceEntryInDir(dir string, filter string) {
+	c := make(chan string)
+	go IterateFilesInDir(dir, filter, c)
+	lst := list.New()
+	for f := range c {
+		evtChan := make(chan InstanceUsage)
+		endChan := make(chan bool)
+		lst.PushBack(endChan)
+		go ParseInstanceUsage(evtChan, GenerateGzipScanner(f))
+		go GenerateHistogramFromStream(evtChan, endChan)
+	}
+
+	for ch := lst.Front(); ch != nil; ch = ch.Next() {
+		realChan, _ := ch.Value.(chan bool)
+		<-realChan
+	}
+
+}
+func main() {
+	ProcessEachInstanceEntryInDir("../data/clusterdata_2019_a/", "instance_usage")
 }
