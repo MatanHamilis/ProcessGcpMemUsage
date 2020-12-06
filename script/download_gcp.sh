3b8f07ddea1a10f96e4fbb3710e671df1d2aba11
Matan Hamilis <matan.hamilis@gmail.com>
2020-12-06 21:48:47 +0200

    updated files

--- script/download_gcp.sh
+++ script/download_gcp.sh
@@ -2,9 +2,11 @@ if [ $(ls -l | grep 'download_gcp.sh' | wc -l) = "0" ]; then
 	echo 'cd to the directory before sourcing!'
 else
 	echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-	sudo apt-get install apt-transport-https ca-certificates gnupg golang
+	sudo apt update
+	sudo apt-get install -y apt-transport-https ca-certificates gnupg golang wondershaper
 	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-	sudo apt-get update && sudo apt-get install google-cloud-sdk
+	sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+	sudo wondershaper eno1 400000 400000
 	gsutil -m cp -r gs://clusterdata_2019_a/ ../data
 	echo 'export GOPATH=~/go' >> ~/.bashrc
 	export GOPATH=~/go
